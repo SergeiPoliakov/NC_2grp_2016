4178d16159481f0629352d8221f17bfd00d9540f
ebloodymir <bedrrin11@gmail.com>
2017-02-13 02:46:33 +0300

    script fix

--- eav/src/main/java/dbHelp/DBHelp.java
+++ eav/src/main/java/dbHelp/DBHelp.java
@@ -1277,11 +1277,11 @@ public class DBHelp {
 
     public static void main(String[] args) throws SQLException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
 
-        User user = new User();
+        /*User user = new User();
         user.setId(10003);
         Meeting meeting = new Meeting("Название", "02.02.2017 16:45", "02.02.2017 22:45", "Информация я написал", user, "Привяу", "");
         System.out.println();
-        new DBHelp().setMeeting(meeting);
+        new DBHelp().setMeeting(meeting);*/
         //ArrayList<User> oldusers = new DBHelp().getUsersAtMeeting("28"); // ID
 
         //System.out.print("dsd");
